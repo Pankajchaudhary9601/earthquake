0544754d793f914610eba1d84971bce87238ed12
Pankajchaudhary9601 <147043248+Pankajchaudhary9601@users.noreply.github.com>
2023-10-09 16:00:53 +0530

    version_1

--- project.py
+++ project.py
@@ -5,7 +5,7 @@ import sklearn
 
 model_clone =joblib.load('earthquake_classification_model.pkl')
 
-testing_data = [[  7.8  ,   4.   ,   7.   ,  20.   , 946.   , 379.   ,   0.   , 23.   ,  20.1  ,  -3.487, 100.082, 925.   , 830.   ,   0.   ,9.   ,   7.   , 439.   ,   1.   ]]
+testing_data = [[     4.   ,   7.   ,  20.   , 946.   , 379.   ,   0.   , 23.   ,  20.1  ,  -3.487, 100.082, 925.   , 830.   ,   0.   ,9.   ,   7.   , 439.   ,   1.   ]]
 
 
 y_pred=model_clone.predict(testing_data)
